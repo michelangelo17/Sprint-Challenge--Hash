8436816de75f75466919d293320adf5f9472645f
michelangelo17 <michelangelo.markus@gmail.com>
2020-05-08 09:30:05 -0700

    finished ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,17 @@
 def has_negatives(a):
-
-    """
-    YOUR CODE HERE
-    """
-
+    result = []
+    neg_dict = {}
+    pos_dict = {}
+    for i in a:
+        if i < 0:
+            neg_dict[i] = i*-1
+        else:
+            pos_dict[i] = i
+    for neg_inverse in neg_dict.values():
+        if neg_inverse in pos_dict:
+            result.append(pos_dict[neg_inverse])
     return result
 
 
 if __name__ == "__main__":
-    print(has_negatives([-1,-2,1,2,3,4,-4]))
+    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
